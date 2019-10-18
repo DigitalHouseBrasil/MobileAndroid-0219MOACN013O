371c10fa5e0491920de0109034c5d42215f82fb8
tairo.miguel <tairo.miguel@zanthus.com>
2019-10-18 11:08:36 -0300

    Update RxJava com comentários

--- Android/RxJava/app/src/main/java/br/com/digitalhouse/rxjava/MainActivity.java
+++ Android/RxJava/app/src/main/java/br/com/digitalhouse/rxjava/MainActivity.java
@@ -1,7 +1,6 @@
 package br.com.digitalhouse.rxjava;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,9 +18,6 @@ import java.util.List;
 import br.com.digitalhouse.rxjava.adapters.UsuarioRecyclerViewAdapter;
 import br.com.digitalhouse.rxjava.model.Usuario;
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -68,68 +64,126 @@ public class MainActivity extends AppCompatActivity {
         // ---- Observable.create
 
 
-        Observable.range(0, 100)
-                .map(numero -> numero * 2)
-                .filter(numero -> numero == 4)
-                .subscribe(integer -> {
-                    System.out.println(integer);
-                },throwable -> {
-                    System.out.println("Error:"  + throwable.getMessage());
-                });
+        // Observable é um objeto que trabalha com emissão de eventos, ou seja,
+        // ele emite estados em que se encontra, para que estes estados possam ser observados por outros elementos,
+        // Ex: o subscribe
 
-        Observable.fromIterable(Arrays.asList("Tairo", "João"))
-                .filter(nome -> nome.equals("Tairo"))
-                .subscribe(n -> {
-                    System.out.println(n);
-                },throwable -> {
-                    System.out.println("Error:"  + throwable.getMessage());
+        // Criando um observable e fazendo o subscribe
+        Observable.range(0, 100) // Emite uma sequencia de inteiros em um range especificado.
+                .map(numero -> numero * 2) // mapeia cada evento emitido multiplicando seu valor por 2
+                .filter(numero -> numero % 2 == 0) // filtra os eventos que tiverem valor par
+                .subscribe(numero -> {
+                    System.out.println(numero); // imprime no console o valor do evento que foi observado
                 });
 
 
-        Observable.just("Tairo")
-                .subscribe(n -> {
-                    System.out.println(n);
-                },throwable -> {
-                    System.out.println("Error:"  + throwable.getMessage());
-                });
+        Observable.just(1, 2) // emite 2 eventos
+                .subscribe(System.out::println);
 
-        Observable<String> stringObservable = Observable.create(emitter -> {
-            emitter.onNext("Tairo");
-            emitter.onNext("João");
-            emitter.onComplete();
+        // Emitindo eventos de erro
+        Observable.error(new NullPointerException("Não pode ser nulo"));
+        //.subscribe(System.out::println);
+
+        // Emitindo eventos apartir de uma lista, e filtrando o nome
+        Observable.fromIterable(Arrays.asList("Tairo", "Jessica", "João"))
+                .filter(s -> s.equals("Tairo"))
+                .map(s -> "Nome: " + s)
+                .subscribe(System.out::println);
+
+        // Cold/Lazy observable -> não emitem eventos enquanto não tiverem atrelado a o um subscriber
+        // Ou seja se não tiver niguem observando eles, eles não fazem nada.
+
+        // Hot observable emite os eventos assim qeu é criado
+        Observable.fromIterable(Arrays.asList("Tairo", "Jessica", "Tadashi"))
+                .filter(s -> s.equals("Tairo"))
+                .map(s -> "Nome: " + s);
+
+
+        // Forma mas clara e dinamica de criar Observables completos é usando o create
+        Observable<Integer> integerObservable = Observable.create(emitter -> {
+            emitter.onNext(1);
+            emitter.onNext(2);
+            emitter.onNext(3);
+            //emitter.onError(new Exception("Deu ruim !"));
+            //emitter.onComplete();
+
+            // buscar na base de dados e emintir cada item no onNext
+            // Aqui poderiamos colocar try/catch e colocar um aexceção no onError
+            // e depois chamar o onConplete quando acabassemos
         });
 
-        stringObservable
-                .map(nome-> {
-                    if (nome.equals("João")){
-                        nome = "Jessica";
-                    }
-                    return nome;
+
+        // Subscribe apenas com onNext
+        integerObservable.subscribe(integer -> {
+            System.out.println(integer);
+        });
+
+        // Subscribe apenas com onNext e onError
+        integerObservable.subscribe(integer -> {
+            System.out.println(integer);
+        }, throwable -> {
+            System.out.println(throwable.getMessage());
+        });
+
+        // Subscribe apenas com onNext, onError e onComplete
+        integerObservable.subscribe(integer -> {
+            System.out.println(integer);
+        }, throwable -> {
+            System.out.println(throwable.getMessage());
+        }, () -> {
+            System.out.println("Completou, não emite mais eventos");
+        });
+
+
+        /// OPERADORES -> formas de interceptar os eventos emitidos
+        // filter -> filtra os eventos baseado em um condição
+        // skip -> pula eventos
+        // map -> maeia cada evento, podendo transformar seu valor
+        // flatmap
+
+        // Flatmap usado quando quero depois da amissão de um evento quisermos retornar um novo observable
+        Observable<String> stringObservable = Observable.just("Aqui é um teste com flatmap");
+        Observable.just(10)
+                .flatMap(numero -> {
+                    System.out.println("Nostrando o número: " + numero);
+                    return stringObservable;
                 })
-                .filter(nome -> nome.equals("Jessica"))
+                .subscribe(System.out::println);
+
+        // So são executados quando há um subscribre
+
+
+        // Schedulers
+        Observable.just(10)
+                .subscribeOn(Schedulers.io())
+                .flatMap(numero -> {
+                    System.out.println("Thread:" + Thread.currentThread().getName());
+                    return stringObservable;
+                })
+                .subscribeOn(Schedulers.newThread())
                 .subscribe(s -> {
-                    // sucesso
-                }, throwable -> {
-                    //erro
-                },() -> {
-                    // completou
+                    System.out.println("Texto do numero: " + s);
                 });
 
+
+        //Aqui buscamos nossos usuários em um método qeu retorna um Observable com um lista de Usuario
         getUsuarios()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe(disposable -> {
+                .subscribeOn(Schedulers.io()) // subscribeOn, indica em que Thread será realizado o trabalho
+                .observeOn(AndroidSchedulers.mainThread()) //observeOn. indica em Thread será mostrado os dados apos serem buscados/processados
+                .doOnSubscribe(disposable -> { // doOnSubscribe, acontece sempre que o observable começa a emitor os eventos
                     // loading = true
                 })
-                .doAfterTerminate(() -> {
+                .doAfterTerminate(() -> { // doAfterTerminate, acontece sempre que o Observable acabou de emitir os eventos
                     // loading = false
                 })
                 .subscribe(usuarios -> {
-                    // Sucesso
+                    // Sucesso, recebe os evento emitidos pelo Observable
                     adapter.update(usuarios);
                 }, throwable -> {
                     // Erro
                     Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                },() -> {
+                    // Complete, completou sem erro
                 });
     }
 
