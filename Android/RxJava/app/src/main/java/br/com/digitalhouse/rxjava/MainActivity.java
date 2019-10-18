1e154ccc67e9f2111a725de516599ef26519a4df
tairoroberto <tairoroberto@hotmail.com>
2019-10-17 22:35:58 -0300

    Add RxJava

--- Android/RxJava/app/src/main/java/br/com/digitalhouse/rxjava/MainActivity.java
+++ Android/RxJava/app/src/main/java/br/com/digitalhouse/rxjava/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.digitalhouse.rxjava;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,9 +13,17 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import br.com.digitalhouse.rxjava.adapters.UsuarioRecyclerViewAdapter;
 import br.com.digitalhouse.rxjava.model.Usuario;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,12 +45,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Você clicou no botão", Snackbar.LENGTH_SHORT).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Você clicou no botão", Snackbar.LENGTH_SHORT).show());
 
         // Streams
         // -- Java 8
@@ -63,5 +67,84 @@ public class MainActivity extends AppCompatActivity {
         // ---- Observable.fromIterable
         // ---- Observable.create
 
+
+        Observable.range(0, 100)
+                .map(numero -> numero * 2)
+                .filter(numero -> numero == 4)
+                .subscribe(integer -> {
+                    System.out.println(integer);
+                },throwable -> {
+                    System.out.println("Error:"  + throwable.getMessage());
+                });
+
+        Observable.fromIterable(Arrays.asList("Tairo", "João"))
+                .filter(nome -> nome.equals("Tairo"))
+                .subscribe(n -> {
+                    System.out.println(n);
+                },throwable -> {
+                    System.out.println("Error:"  + throwable.getMessage());
+                });
+
+
+        Observable.just("Tairo")
+                .subscribe(n -> {
+                    System.out.println(n);
+                },throwable -> {
+                    System.out.println("Error:"  + throwable.getMessage());
+                });
+
+        Observable<String> stringObservable = Observable.create(emitter -> {
+            emitter.onNext("Tairo");
+            emitter.onNext("João");
+            emitter.onComplete();
+        });
+
+        stringObservable
+                .map(nome-> {
+                    if (nome.equals("João")){
+                        nome = "Jessica";
+                    }
+                    return nome;
+                })
+                .filter(nome -> nome.equals("Jessica"))
+                .subscribe(s -> {
+                    // sucesso
+                }, throwable -> {
+                    //erro
+                },() -> {
+                    // completou
+                });
+
+        getUsuarios()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(disposable -> {
+                    // loading = true
+                })
+                .doAfterTerminate(() -> {
+                    // loading = false
+                })
+                .subscribe(usuarios -> {
+                    // Sucesso
+                    adapter.update(usuarios);
+                }, throwable -> {
+                    // Erro
+                    Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    private Observable<List<Usuario>> getUsuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("Tairo", 15));
+        usuarios.add(new Usuario("Jésica", 18));
+        usuarios.add(new Usuario("João", 13));
+        usuarios.add(new Usuario("César", 18));
+        usuarios.add(new Usuario("Edney", 27));
+        usuarios.add(new Usuario("Tairo", 15));
+        usuarios.add(new Usuario("Tairo", 15));
+        usuarios.add(new Usuario("Tairo", 15));
+        usuarios.add(new Usuario("Tairo", 15));
+
+        return  Observable.just(usuarios);
     }
 }
